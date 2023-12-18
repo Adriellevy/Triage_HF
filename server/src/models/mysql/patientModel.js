353eb4d4912d1d83a28ff2785edaee268e4b63cc
Lucas Gomez <lucasgomezoliva@gmail.com>
2023-12-18 14:59:56 -0300

    Modify patientmodel

--- server/src/models/mysql/patientModel.js
+++ server/src/models/mysql/patientModel.js
@@ -3,22 +3,27 @@ import { connection } from '../../db.js'
 export class PatientsModel {
   static async getAllPatients() {
     const patientsQuery = `
-    SELECT Patient.*, Doctor.doctor_name, Nurse.nurse_name
+    SELECT Patient.*, 
+    Doctor.user_name AS doctor_name,
+    Nurse.user_name AS nurse_name
     FROM Patient
-    LEFT JOIN Doctor ON Patient.doctor_id = Doctor.doctor_id
-    LEFT JOIN Nurse ON Patient.nurse_id = Nurse.nurse_id
-  `
+    LEFT JOIN Users AS Doctor ON Patient.doctor_id = Doctor.user_id AND Doctor.user_type = 'DOCTOR'
+    LEFT JOIN Users AS Nurse ON Patient.nurse_id = Nurse.user_id AND Nurse.user_type = 'NURSE';
+    `
+
     const [patients] = await connection.query(patientsQuery)
     return patients
   }
 
   static async getPatientById({ id }) {
     const patientsQuery = `
-      SELECT Patient.*, Doctor.doctor_name, Nurse.nurse_name
-      FROM Patient
-      LEFT JOIN Doctor ON Patient.doctor_id = Doctor.doctor_id
-      LEFT JOIN Nurse ON Patient.nurse_id = Nurse.nurse_id
-      WHERE Patient.patient_id = ?
+    SELECT Patient.*, 
+    Doctor.user_name AS doctor_name,
+    Nurse.user_name AS nurse_name
+    FROM Patient
+    LEFT JOIN Users AS Doctor ON Patient.doctor_id = Doctor.user_id AND Doctor.user_type = 'DOCTOR'
+    LEFT JOIN Users AS Nurse ON Patient.nurse_id = Nurse.user_id AND Nurse.user_type = 'NURSE'
+    WHERE Patient.patient_id = ?;
     `
     const [patients] = await connection.query(patientsQuery, [id])
     if (patients.length === 0) return false
