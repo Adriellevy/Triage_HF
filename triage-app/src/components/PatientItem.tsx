ad95688d4769ee224058bd9c1e2861174f0b5d85
Franco Nebro <franconp02@gmail.com>
2024-01-29 22:03:17 -0300

    cambio en visualizacion de fechas, adaptacion tamaño pantalla responsive

--- triage-app/src/components/PatientItem.tsx
+++ triage-app/src/components/PatientItem.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import { useState, useEffect } from 'react'
 import { Patient } from '../interfaces/Patinet'
 
 interface PropsPatientItem {
@@ -7,7 +8,10 @@ interface PropsPatientItem {
   onViewDetails: (patient: Patient) => void
 }
 
-function PatientItem({ patient, onDelete, onViewDetails }: PropsPatientItem) {
+function PatientItem({ patient, onViewDetails }: PropsPatientItem) {
+  const [age, setAge] = useState<number | null>(null)
+  const [entryTime, setEntryTime] = useState<string | null>(null)
+
   const {
     patient_name,
     patient_id,
@@ -21,20 +25,41 @@ function PatientItem({ patient, onDelete, onViewDetails }: PropsPatientItem) {
     nurse_name,
     patient_status
   } = patient
+
+  useEffect(() => {
+    const birthDate = new Date(date_of_birth)
+    const actualDate = new Date()
+    setAge(actualDate.getFullYear() - birthDate.getFullYear())
+
+    const fechaOriginal = new Date('2023-01-01T11:00:00.000Z')
+
+    //format options
+    const dateFormat = {
+      year: 'numeric',
+      month: 'long',
+      day: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      // second: 'numeric',
+      hour12: true
+    }
+    const formatoFechaHora = new Intl.DateTimeFormat('es-ES', dateFormat)
+    const fechaFormateada = formatoFechaHora.format(fechaOriginal)
+    setEntryTime(fechaFormateada)
+  }, [])
+
   return (
     <tr className='w-full'>
-      <td className={`border text-sm overflow-hidden `}>
-        {patient_name}
-        </td>
-      <td className='border p-2 hidden lg:table-cell'>{date_of_birth}</td>
-      <td className='border p-2 hidden lg:table-cell'>{entry_time}</td>
-      <td className='border md:p-2'>{patient_triage_level}</td>
-      <td className='border p-2 hidden lg:table-cell'>{patient_medication}</td>
-      <td className='border p-2 hidden lg:table-cell'>{patient_problem}</td>
-      <td className='border md:p-2'>ID:{box_id}</td>
-      <td className='border p-2 hidden lg:table-cell'>{doctor_name}</td>
-      <td className='border p-2 hidden lg:table-cell'>{nurse_name}</td>
-      <td className='border text-sm md:p-2'>{patient_status}</td>
+      <td className={`border text-sm overflow-hidden text-center `}>{patient_name}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{age}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{entryTime}</td>
+      <td className='border md:p-2 text-center'>{patient_triage_level}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{patient_medication}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{patient_problem}</td>
+      <td className='border md:p-2 text-center'>ID:{box_id}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{doctor_name}</td>
+      <td className='border p-2 hidden lg:table-cell text-center'>{nurse_name}</td>
+      <td className='border text-sm md:p-2 text-center'>{patient_status}</td>
       <td className='border p-2'>
         <button
           onClick={() => onViewDetails(patient)}
