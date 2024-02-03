84e5c38218ece958e2a8d0c8c7d7ddb1a7cff4ae
adriel levy <adriellevy2014@hotmail.com>
2024-02-03 14:55:52 -0300

    SelectorMenu nuevamentea arreglado

--- triage-app/src/components/SelectorMenu.tsx
+++ triage-app/src/components/SelectorMenu.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 
 interface Option {
   label: string
@@ -12,14 +12,69 @@ interface SelectorMenuProps {
 
 const SelectorMenu: React.FC<SelectorMenuProps> = ({ options, onSelect }) => {
   const [selectedOption, setSelectedOption] = useState<number | null>(null)
+  const [sidebarVisible, setSidebarVisible] = useState(true)
+  const sidebarRef = useRef<HTMLDivElement>(null)
+  useEffect(() => {
+    const handleResize = () => {
+      if (sidebarRef.current) {
+        const itemCount = options.length
+        const minWidth = 40 // Minimum width for the selector
+        const maxWidth = 300 // Maximum width for the selector
+
+        const calculatedWidth = Math.min(
+          minWidth + (itemCount - 1) * 20, // Adjust 30 as needed based on your design
+          maxWidth
+        )
+
+        if (window.innerWidth <= 767) {
+          sidebarRef.current.style.bottom = '0'
+          sidebarRef.current.style.maxWidth = '100%'
+          sidebarRef.current.style.left = '0%'
+          sidebarRef.current.style.right = '0%'
+        } else if (window.innerWidth <= 1015 && window.innerWidth >= 767) {
+          sidebarRef.current.style.position = 'fixed'
+          sidebarRef.current.style.bottom = '0'
+          sidebarRef.current.style.left = '28%'
+        } else if (window.innerWidth <= 1100) {
+          setSidebarVisible(false)
+          sidebarRef.current.style.position = 'fixed'
+          sidebarRef.current.style.bottom = '0'
+          sidebarRef.current.style.left = '22%'
+        } else {
+          setSidebarVisible(true)
+          sidebarRef.current.style.position = 'static'
+          sidebarRef.current.style.bottom = 'unset'
+          sidebarRef.current.style.width = 'auto'
+          sidebarRef.current.style.left = '28%'
+          sidebarRef.current.style.right = '0%'
+        }
+      }
+    }
+
+    window.addEventListener('resize', handleResize)
+    handleResize()
+
+    return () => {
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
 
   return (
-    <div className='selector-menu-container ml-4 w-40 border-r border-gray-300 p-2 bg-white rounded-lg shadow-md'>
-      <div className='selector-menu'>
+    <div
+      ref={sidebarRef}
+      className={`selector-menu-container ${
+        sidebarVisible ? 'mx-auto ml-4' : 'fixed bottom-0 w-full pl-5'
+      } w-40 border-r border-gray-300 p-2 bg-white rounded-lg shadow-md`}
+    >
+      <div
+        className={`selector-menu ${
+          sidebarVisible ? 'flex flex-col' : 'flex flex-row overflow-x-auto'
+        }`}
+      >
         {options.map((option, index) => (
           <div
             key={index}
-            className={`selector-option flex items-center p-2 mt-2 cursor-pointer transition duration-200  ${
+            className={`selector-option flex items-center p-2 mt-2 cursor-pointer transition duration-200 mr-2  ${
               selectedOption === index
                 ? 'active bg-blue-500 text-white'
                 : 'bg-gray-100 hover:bg-gray-200'
@@ -30,7 +85,7 @@ const SelectorMenu: React.FC<SelectorMenuProps> = ({ options, onSelect }) => {
             }}
           >
             <div className='relative w-8 h-8 mr-2'>
-              <div className='absolute -bottom-4 left-0 right-0 top-4 transform -translate-y-1/3 bg-gray-200 rounded-full border-2'>
+              <div className='absolute -bottom-4 left-0 right-0 top-4 transform -translate-y-1/3 bg-gray-200 rounded-full border-2 '>
                 <img
                   src={option.image}
                   alt={option.label}
