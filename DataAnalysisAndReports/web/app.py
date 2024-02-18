c014827c473fd38640eeacb815712ac294e1a080
just-juanma <jugoka947@gmail.com>
2024-02-18 13:50:32 -0300

    first web deployment

---
+++ DataAnalysisAndReports/web/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+
+import plotly.graph_objects as go
+
+app = Flask(__name__)
+
+@app.route('/grafico')
+def mostrar_grafico():
+    # Datos de ejemplo (reemplázalos con tus propios datos)
+    x = [1, 2, 3, 4, 5]
+    y = [10, 11, 9, 12, 8]
+
+    # Crear el gráfico
+    scatter_plot = go.Scatter(x=x, y=y, mode='markers', name='Puntos')
+
+    # Configurar el diseño del gráfico
+    layout = go.Layout(title='Mi Gráfico de Dispersión', xaxis=dict(title='Eje X'), yaxis=dict(title='Eje Y'))
+
+    # Crear la figura
+    fig = go.Figure(data=[scatter_plot], layout=layout)
+
+    # Renderizar el gráfico en HTML
+    return fig.to_html()
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
