b440f9c0ae357831b5b41bf5041f03fbe652c6a6
just-juanma <jugoka947@gmail.com>
2024-02-20 10:57:02 -0300

    first deployment of own graphics to web the modules app.py and the index.html file were moved to the main path visualization content added in visualize.py fixes in build_features.py

---
+++ DataAnalysisAndReports/src/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+
+from data.make_dataset import get_df
+from features.build_features import build_features
+from visualization.visualize import cant_pacientes_fecha
+from visualization.visualize import bar_chart
+
+app = Flask(__name__)
+
+
+@app.route('/grafico')
+def mostrar_grafico():
+    df = get_df()
+    df = build_features(df)
+    df = cant_pacientes_fecha(df)
+    fig = bar_chart(df = df, 
+               x='FECHA DE INGRESO', y='CANTIDAD DE PACIENTES', 
+               x_title='Cantidad de Pacientes por Fecha de Ingreso', y_title='Fecha de Ingreso', title='Cantidad de Pacientes', 
+               mean=True)
+    return fig.to_html()
+
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
