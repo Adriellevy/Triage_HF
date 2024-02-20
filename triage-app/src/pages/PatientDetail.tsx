86bce7fe13796ded59faed73474deafba011d8ca
Lucas <lucasgomezoliva@gmail.com>
2024-02-19 21:04:04 -0300

    Add LoaderSpin PatientDetail

--- triage-app/src/pages/PatientDetail.tsx
+++ triage-app/src/pages/PatientDetail.tsx
@@ -5,6 +5,7 @@ import { getPatientById } from '@/services/patientService'
 import { updatePatient } from '@/services/patientService'
 import { consulta } from '@/services/openai-test'
 import { Button } from '@/components/ui'
+import LoaderSpin from '@/components/LoaderSpin'
 
 const solicitud =
   'Toma el rol de un médico cardiólogo que escribe de forma resumida las evoluciones de sus pacientes. Crea un resumen de 5 líneas en primera persona del singular. Muy resumido. Únicamente puntos importantes:  Paciente masculino 47 años Trabaja en comercio  Antec: IAM con SDST 2021. PTCA a ADA prox con un DES. FE 40%. Hipertensión arterial, Hipotiroidismo, Insulinoresistencia, Alergias: -, Tabaco: -  AAFF: Hermano IAM reciente  Medicamentos: AAS 100x1, Clop 75x1, Atorvastatina 20x4, Eutirox 75, bisoprolol 2.5x1, espironolactona 12.5x1, Metformina XR 750x1, Clotiazepam 5x1, Ezetimibe 10x1, Setralina 50x1,Hospitalizacion reciente por COVID Desde el alta con dolor torácico, constanteAl examen: EVA 0/10 PA 100/60 FC 80  Yug planas, sin soplos carotideos  RR2TSS  MP+SRA  Abd: BDI, no palpo masas ni visceromegalias, Ao impresiona de tamaño normal  Piel tibia a distal sin edema, pulsos simétricosPlan: Suspender clopidogrel Eco y test esfuerzo Control con resultado. Ahora cambia lo que creas necesario por la informacion de este paciente:'
@@ -99,10 +100,7 @@ function PatientDetail() {
     const result = await consulta(prompt)
     setLoading(false)
     const requestInfo = result && result.message && result.message.content
-    console.log(requestInfo)
-    const requestInfoElement = document.getElementById('request-info') as HTMLElement
-    if (requestInfoElement) {
-      requestInfoElement.innerText = requestInfo || 'No information available'
+    if (requestInfo) {
       setInform(requestInfo)
     } else {
       console.error('Element with class "text-gray-700" not found')
@@ -165,40 +163,18 @@ function PatientDetail() {
                 <h3 className={`text-lg font-bold mb-2 ${loading ? 'hidden' : ''} `}>
                   Requested Inform:
                 </h3>
-                <p className={`text-gray-700 ${loading ? 'hidden' : ''}`} id='request-info'>
-                  {loading ? 'Loading...' : Inform || 'Not able to charge the inform'}
-                </p>
-                <div className={`loader-container ${loading ? '' : 'hidden'}`}>
-                  <div className='loader' />
-                </div>
+                {loading ? (
+                  <div className='flex justify-center items-center'>
+                    <LoaderSpin />
+                  </div>
+                ) : (
+                  <p className='text-gray-700' id='request-info'>
+                    {Inform}
+                  </p>
+                )}
               </div>
             )}
           </div>
-          <style>
-            {`
-        @keyframes spin {
-          0% { transform: rotate(0deg); }
-          100% { transform: rotate(360deg); }
-        }
-        .loader-container {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        .loader {
-          border: 4px solid #f3f3f3;
-          border-top: 4px solid #3498db;
-          border-radius: 50%;
-          width: 30px;
-          height: 30px;
-          animation: spin 1s linear infinite;
-        }
-
-        .hidden {
-          display: none;
-        }
-      `}
-          </style>
         </>
       )}
     </div>
