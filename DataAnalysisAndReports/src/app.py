eb2bb6a0781f2083918df90f83c580eeffcef05e
just-juanma <jugoka947@gmail.com>
2024-02-23 12:40:16 -0300

    app parameters modified

--- DataAnalysisAndReports/src/app.py
+++ DataAnalysisAndReports/src/app.py
@@ -7,35 +7,47 @@ from visualization.visualize import bar_chart
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def mostrar_grafico():
-    desde = request.args.get('desde', default=None, type=str)
-    hasta = request.args.get('hasta', default=None, type=str)
-    nombreyapellido = request.args.get('nombreyapellido', default=None, type=str)
-    triage = request.args.get('triage', default=None, type=str)
-    medico = request.args.get('medico', default=None, type=str)
-    alta = request.args.get('alta', default=None, type=str)
-
-    print("Desde:", desde)
-    print("Hasta:", hasta)
-    print("Nombre y Apellido:", nombreyapellido)
-    print("Triage:", triage)
-    print("Médico:", medico)
-    print("Alta:", alta)
-
+def grafico_cant_pacientes_fecha():
     df = get_df()
     df = build_features(df)
-    df = cant_pacientes_fecha(df = df)
+
+    print(df)
+
+    desde = request.args.get('desde', default=None, type=str)
+    hasta = request.args.get('hasta', default=None, type=str)
+    nombre_y_apellido = request.args.get('nombreyapellido', default=None, type=str)
+    motivo_de_consulta = request.args.get('motivodeconsulta', default=None, type=str)
+    box = request.args.get('box', default=None, type=int)
+    triage = request.args.get('triage', default=None, type=int)
+    medico = request.args.get('medico', default=None, type=str)
+    enfermero = request.args.get('enfermero', default=None, type=str)
+    alta = request.args.get('alta', default=None, type=str)
+    aislado = request.args.get('aislado', default=None, type=bool)
+
+    print(f"desde: {desde}")
+    print(f"hasta: {hasta}")
+    print(f"nombre_y_apellido: {nombre_y_apellido}")
+    print(f"motivo_de_consulta: {motivo_de_consulta}")
+    print(f"box: {box}")
+    print(f"triage: {triage}")
+    print(f"medico: {medico}")
+    print(f"enfermero: {enfermero}")
+    print(f"alta: {alta}")
+    print(f"aislado: {aislado}")
+
+
+    df = cant_pacientes_fecha(df, desde, hasta, nombre_y_apellido, motivo_de_consulta, box, triage, medico, enfermero, alta, aislado)
+
+    media = request.args.get('media', default=None, type=bool)
 
     # Dejar fijo
     fig = bar_chart(df = df, 
                x='FECHA DE INGRESO', y='CANTIDAD DE PACIENTES', 
                x_title='Cantidad de Pacientes por Fecha de Ingreso', y_title='Fecha de Ingreso', title='Cantidad de Pacientes', 
-               mean=True)
-    return fig.to_html()
-
+               mean = media)
     
+    return fig.to_html()
 
 if __name__ == '__main__':
     app.run(debug=True)
