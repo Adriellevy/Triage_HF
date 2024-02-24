00e0a637dc75740efe756642ef5c9ce8c3dfceab
just-juanma <jugoka947@gmail.com>
2024-02-23 22:54:38 -0300

    structural reorganization of the code get_args added df as global variable intilialize_data added

--- DataAnalysisAndReports/src/app.py
+++ DataAnalysisAndReports/src/app.py
@@ -7,45 +7,36 @@ from visualization.visualize import bar_chart
 
 app = Flask(__name__)
 
-@app.route('/')
-def grafico_cant_pacientes_fecha():
+#
+# Arreglar problemas con los filtros de las fechas
+#
+
+def initialize_data():
     df = get_df()
     df = build_features(df)
+    return df
 
-    desde = request.args.get('desde', default=None, type=str)
-    hasta = request.args.get('hasta', default=None, type=str)
-    nombre_y_apellido = request.args.get('nombreyapellido', default=None, type=str)
-    if(nombre_y_apellido):
-        nombre_y_apellido = nombre_y_apellido.upper()
-    motivo_de_consulta = request.args.get('motivodeconsulta', default=None, type=str)
-    if(motivo_de_consulta):
-        motivo_de_consulta = motivo_de_consulta.upper()
-    box = request.args.get('box', default=None, type=int)
-    triage = request.args.get('triage', default=None, type=float)
-    medico = request.args.get('medico', default=None, type=str)
-    if(medico):
-        medico = medico.upper()
-    enfermero = request.args.get('enfermero', default=None, type=str)
-    if(enfermero):
-        enfermero = enfermero.upper()
-    alta = request.args.get('alta', default=None, type=str)
-    if(alta):
-        alta = alta.upper()
-    aislado = request.args.get('aislado', default=None, type=bool)
+df = initialize_data()
 
-    print(f"desde: {desde}")
-    print(f"hasta: {hasta}")
-    print(f"nombre_y_apellido: {nombre_y_apellido}")
-    print(f"motivo_de_consulta: {motivo_de_consulta}")
-    print(f"box: {box}")
-    print(f"triage: {triage}")
-    print(f"medico: {medico}")
-    print(f"enfermero: {enfermero}")
-    print(f"alta: {alta}")
-    print(f"aislado: {aislado}")
+def get_args():
+    args = { 'desde': request.args.get('desde', default=None, type=str),
+             'hasta': request.args.get('hasta', default=None, type=str),
+             'nombre_y_apellido': request.args.get('nombreyapellido', default=None, type=str),
+             'motivo_de_consulta': request.args.get('motivodeconsulta', default=None, type=str),
+             'box': request.args.get('box', default=None, type=int),
+             'triage': request.args.get('triage', default=None, type=float),
+             'medico': request.args.get('medico', default=None, type=str),
+             'enfermero': request.args.get('enfermero', default=None, type=str),
+             'alta': request.args.get('alta', default=None, type=str),
+             'aislado': request.args.get('aislado', default=None, type=bool)
+            }
+    return args
 
+@app.route('/')
+def grafico_cant_pacientes_fecha():
+    global df
 
-    df = cant_pacientes_fecha(df, desde, hasta, nombre_y_apellido, motivo_de_consulta, box, triage, medico, enfermero, alta, aislado)
+    df = cant_pacientes_fecha(df, **get_args())
 
     media = request.args.get('media', default=None, type=bool)
 
@@ -59,3 +50,4 @@ def grafico_cant_pacientes_fecha():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    initialize_data()
