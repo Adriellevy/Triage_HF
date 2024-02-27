6674569eb0d256e1574605bc13e8a7a7a0703304
Lucas <lucasgomezoliva@gmail.com>
2024-02-27 15:09:05 -0300

    Change BoxList

--- triage-app/src/components/BoxItem.tsx
+++ triage-app/src/components/BoxItem.tsx
@@ -4,36 +4,39 @@ import { Link } from 'react-router-dom'
 
 interface PropsBoxItem {
   box: Box
+  index: number
 }
 
-function BoxItem({ box }: PropsBoxItem) {
+function BoxItem({ box, index }: PropsBoxItem) {
   const { patient_id, patient_name, box_code, box_type, box_status } = box
+  const isOdd = index % 2 !== 0
+  const bgClass = isOdd ? 'bg-white' : 'bg-gray-100'
 
   return (
-    <div className='border p-4 mb-4 rounded-md'>
-      <h2 className='text-xl font-bold mb-2'>Box {box_code}</h2>
-      <p>
-        <strong>Type:</strong> {box_type}
-      </p>
-      <p>
-        <strong>Status:</strong> {box_status}
-      </p>
-      {box_status === BoxStatus.OCUPADO && (
-        <>
-          <p>
-            <strong>Patient:</strong> {patient_name}
-          </p>
-          <p>
-            <strong>Time:</strong> {box_status}
-          </p>
-          <div>
-            <Link to={`/patients/${patient_id}`}>
-              <Button color='green'>Get patient Info</Button>
-            </Link>
-          </div>
-        </>
-      )}
-    </div>
+    <tr className={bgClass}>
+      <td className='border p-2 '>{box_code}</td>
+      <td className='border p-2 hidden lg:table-cell'>{box_type}</td>
+      <td className='border p-2'>
+        <div
+          className={`rounded-md p-2 ${
+            box_status !== BoxStatus.OCUPADO ? 'bg-green-500 shadow-md' : 'bg-red-500 shadow-md'
+          }`}
+        >
+          {box_status}
+        </div>
+      </td>
+      <td className='border p-2 '>{box_status !== BoxStatus.OCUPADO ? null : patient_name}</td>
+      <td className='border p-2 hidden lg:table-cell'>
+        {box_status !== BoxStatus.OCUPADO ? null : <>tiempo</>}
+      </td>
+      <td className='border p-2 '>
+        {box_status !== BoxStatus.OCUPADO ? null : (
+          <Link to={`/patients/${patient_id}`}>
+            <Button color='green'>Get patient Info</Button>
+          </Link>
+        )}
+      </td>
+    </tr>
   )
 }
 
--- triage-app/src/components/BoxList.tsx
+++ triage-app/src/components/BoxList.tsx
@@ -9,12 +9,37 @@ function BoxList({ boxes }: PropsBoxList) {
   if (boxes === null) {
     return <div>Cargando...</div>
   }
-  return (
+  {
+    /*
+    return (
     <div className='mt-4 mx-8'>
       <h1 className='text-2xl font-bold mb-4'>Boxes List</h1>
       {boxes.map((box, index) => (
         <BoxItem key={index} box={box} />
       ))}
+    </div>
+  )*/
+  }
+
+  return (
+    <div className='mx-0 mt-4 lg:mx-8'>
+      <table className='w-full border border-gray-300'>
+        <thead>
+          <tr className='min-w-full bg-blue-400'>
+            <th className='border p-2 '>Code</th>
+            <th className='border p-2 hidden lg:table-cell'>Type</th>
+            <th className='border p-2 '>Status</th>
+            <th className='border p-2 '>Patient</th>
+            <th className='border p-2 hidden lg:table-cell'>Time</th>
+            <th className='border p-2 '>Action</th>
+          </tr>
+        </thead>
+        <tbody className='text-center'>
+          {boxes.map(
+            (Item, index) => boxes && <BoxItem key={Item.box_id} box={Item} index={index} />
+          )}
+        </tbody>
+      </table>
     </div>
   )
 }
