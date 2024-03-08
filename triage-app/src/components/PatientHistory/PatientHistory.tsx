560ad641fa115939ace5499b8bcf0e2e46a1f9ec
Lucas <lucasgomezoliva@gmail.com>
2024-03-08 17:18:53 -0300

    Add patienthistory translations with i18n

--- triage-app/src/components/PatientHistory/PatientHistory.tsx
+++ triage-app/src/components/PatientHistory/PatientHistory.tsx
@@ -3,12 +3,14 @@ import { PatientHistoryItem } from '@/interfaces/Patinet'
 import PatientHystoryItem from '@/components/PatientHistory/PatientHystoryItem'
 import { getPatientHistory } from '@/services/patientService'
 import LoaderSpin from '../LoaderSpin'
+import { useTranslation } from 'react-i18next'
 
 interface PropsPatienHistory {
   patient_id: string | undefined
 }
 
 function PatientHistory({ patient_id }: PropsPatienHistory) {
+  const { t } = useTranslation('PatientHistory')
   const [PatientHistoryData, setPatientHistoryData] = useState<PatientHistoryItem[]>([])
   const [isLoading, setisLoading] = useState<boolean>(true)
 
@@ -27,7 +29,7 @@ function PatientHistory({ patient_id }: PropsPatienHistory) {
 
   return (
     <div className='mx-0 mt-4 lg:mx-0'>
-      <h3 className='text-xl  font-bold mt-4 mb-6'>Patient Change History</h3>
+      <h3 className='text-xl  font-bold mt-4 mb-6'>{t('title')}</h3>
       <div className='max-h-72 overflow-y-auto'>
         {isLoading ? (
           <div className='flex justify-center items-center'>
@@ -37,11 +39,11 @@ function PatientHistory({ patient_id }: PropsPatienHistory) {
           <table className='w-full border border-gray-300 text-white'>
             <thead>
               <tr className='min-w-full bg-blue-800'>
-                <th className='border p-2 '>Time</th>
-                <th className='border p-2 '>Item</th>
-                <th className='border p-2 '>Before</th>
-                <th className='border p-2 '>After</th>
-                <th className='border p-2 '>Edit by</th>
+                <th className='border p-2 '>{t('TimeLable')}</th>
+                <th className='border p-2 '>{t('ItemLable')}</th>
+                <th className='border p-2 '>{t('BeforeLable')}</th>
+                <th className='border p-2 '>{t('AfterLable')}</th>
+                <th className='border p-2 '>{t('EditByLabel')}</th>
               </tr>
             </thead>
             <tbody className='text-center'>
