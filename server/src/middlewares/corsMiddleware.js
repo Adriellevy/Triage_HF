5370165b6229f344dba5cea90cb72b5d5937d20c
just-juanma <jugoka947@gmail.com>
2024-03-11 09:20:13 -0300

    yesterday connection

--- server/src/middlewares/corsMiddleware.js
+++ server/src/middlewares/corsMiddleware.js
@@ -8,6 +8,7 @@ const ACCEPTED_ORIGINS = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://192.168.100.238:5173',
+  'http://192.168.0.99:5173'
 ]
 
 export const corsMiddleware = ({ acceptedOrigins = ACCEPTED_ORIGINS } = {}) =>
--- triage-app/src/pages/stats/StatsPatinet.tsx
+++ triage-app/src/pages/stats/StatsPatinet.tsx
@@ -1,7 +1,7 @@
 function StatsPatinet() {
   return (
     <iframe
-      src={`http://localhost:5000/cant_pacientes_fecha`}
+      src={`http://127.0.0.1:5000/cant_pacientes_fecha`}
       title='Gráfico'
       width='100%'
       height='100%'
