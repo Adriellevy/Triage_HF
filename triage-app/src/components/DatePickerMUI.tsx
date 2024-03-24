5bb015b666f6f34a0cf85c1caef1b53b91243746
adriel levy <adriellevy2014@hotmail.com>
2024-03-24 16:10:46 -0300

    Merge

--- triage-app/src/components/DatePickerMUI.tsx
+++ triage-app/src/components/DatePickerMUI.tsx
@@ -5,65 +5,72 @@ import { DatePicker } from '@mui/x-date-pickers'
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs'
 import { LocalizationProvider } from '@mui/x-date-pickers'
 import dayjs from 'dayjs'
+import isSameOrBefore from 'dayjs/plugin/isSameOrBefore'
+import isSameOrAfter from 'dayjs/plugin/isSameOrAfter'
 
+dayjs.extend(isSameOrBefore)
+dayjs.extend(isSameOrAfter)
+import { ErrorActive } from '@/interfaces/Errors'
 
 interface DatePickerMUIProps {
   onChangeExt: (newDate: Date | null) => void
-  selectedDateExt: Date | null;
-  error_active:any
+  selectedDateExt: Date | null
+  error_active: ErrorActive
 }
 
-
-export const DatePickerMUI: React.FC<DatePickerMUIProps> = ({ onChangeExt,selectedDateExt,error_active }) => {
+export const DatePickerMUI: React.FC<DatePickerMUIProps> = ({
+  onChangeExt,
+  selectedDateExt,
+  error_active
+}) => {
   const [selectedDate, setSelectedDate] = useState<Date | null>(null)
 
   useEffect(() => {
     // Actualiza el estado local cuando cambia la fecha seleccionada desde el padre
-    setSelectedDate(selectedDateExt);
-  }, [selectedDateExt]);
+    setSelectedDate(selectedDateExt)
+  }, [selectedDateExt])
 
-
-  const getFormatTime = (date: Date | null) => {
+  const getFormatTime = (date: Date | null): Date | null => {
     if (date) {
       const fecha = new Date(date)
       const dia = fecha.getDate().toString().padStart(2, '0') // Obtiene el día y lo convierte a string con dos dígitos
       const mes = (fecha.getMonth() + 1).toString().padStart(2, '0') // Obtiene el mes (los meses empiezan desde 0)
       const anio = fecha.getFullYear()
-      return `${anio}-${mes}-${dia}`
+      return new Date(`${anio}-${mes}-${dia}`)
     }
+    return null
   }
 
   const handleChange = (newDate: Date | null) => {
     setSelectedDate(newDate)
+
     onChangeExt(getFormatTime(newDate))
   }
 
-  const yesterday = dayjs().subtract(130, 'year');
-  const today = dayjs()
+  const yesterday = dayjs().subtract(130, 'year').toDate()
+  const today = dayjs().toDate()
 
   const errorBorder = error_active?.value == true ? 'border-red-500' : ''
   const errorMessage = error_active?.message
 
- 
   return (
     <div>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <div className={`${errorBorder?'redBorderDataPick':''}`}>
-        <DatePicker
-          // label="Seleccionar fecha"
-          views={['year', 'month', 'day']}
-          openTo='year'
-          minDate={yesterday}
-          maxDate={today}
-          value={selectedDate}
-          onChange={handleChange}
-          format='DD/MM/YYYY'
-          className={`w-full h-0 m-0 p-0 text-sm datePick`}
+        <div className={`${errorBorder ? 'redBorderDataPick' : ''}`}>
+          <DatePicker
+            // label="Seleccionar fecha"
+            views={['year', 'month', 'day']}
+            openTo='year'
+            minDate={yesterday}
+            maxDate={today}
+            value={selectedDate}
+            onChange={handleChange}
+            format='DD/MM/YYYY'
+            className={`w-full h-0 m-0 p-0 text-sm datePick`}
           />
-          </div>
+        </div>
       </LocalizationProvider>
       {errorBorder && <span className='text-red-500 relative'>{errorMessage}</span>}
-
     </div>
   )
 }
