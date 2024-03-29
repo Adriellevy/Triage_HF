ce596571d6a18eff15b6d7789d8fa5a9c964e888
Lucas <lucasgomezoliva@gmail.com>
2024-03-29 14:21:59 -0300

    Change stats url

--- triage-app/src/pages/stats/StatsPatinet.tsx
+++ triage-app/src/pages/stats/StatsPatinet.tsx
@@ -1,7 +1,7 @@
 function StatsPatinet() {
   return (
     <iframe
-      src={`http://127.0.0.1:5000/cant_pacientes_fecha`}
+      src={`http://127.0.0.1:5000/number_patients_date/`}
       title='Gráfico'
       width='100%'
       height='100%'
