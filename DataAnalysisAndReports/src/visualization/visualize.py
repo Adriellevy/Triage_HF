bd046689f3f4153c597bad58f8361486ec2fdf34
Juan Manuel <juanmanuelgonzalezkapnik@gmail.com>
2024-03-31 14:21:37 -0300

    number patients date chart update renamed some columns renamed triage levels when display removing unnecessary metrics

--- DataAnalysisAndReports/src/visualization/visualize.py
+++ DataAnalysisAndReports/src/visualization/visualize.py
@@ -9,44 +9,44 @@ import plotly.graph_objects as go
 # showarrow = False,
 # font = dict(color = 'red'))
 
-color_discrete_map = {'1': '#BDBEBE', '2': '#FA8162', '3': '#CCCC52', '4': '#A0C791'}
+color_discrete_map = {'Nivel 1': '#BDBEBE', 
+                      'Nivel II': '#FA8162', 
+                      'Nivel III': '#CCCC52', 
+                      'Nivel IV': '#A0C791'}
 
-def include_mean(fig, df, x, y, mean=None):
+def include_mean(fig, df, x, y):
     df_sum = df.groupby(x)[y].sum().reset_index(name=y)
     mean = df_sum[y].mean()
     fig.add_trace(go.Scatter(x=df_sum[x],
                                  y=[mean] * len(df_sum),
                                  mode='lines',
-                                 name='Media',
+                                 name='Media (Total)',
                                  line=dict(color='red', width=2, dash='dash'),
-                                 hovertemplate='%{y:.2f}'))
+                                 hovertemplate='<b>%{y:.2f} pacientes</b>'))
         
 def update_layout(fig, title, x_title, y_title):
-    fig.update_layout(title=title,
+    fig.update_layout(title=f'<b>{title}</b>',
+                      title_x=0.5,
                       xaxis_title=x_title,
                       yaxis_title=y_title,
-                      title_x=0.5,
                       plot_bgcolor='white',
                       xaxis=dict(linecolor='black', showgrid=True),
                       yaxis=dict(linecolor='black', showgrid=True),
                       legend=dict(title='Nivel de Triage'))
     
-        
-
-def bar_chart(df, x, y, title, x_title, y_title, color, mean=None):
-    global color_discrete_map
-    fig = px.bar(df,
-                 x=x,
-                 y=y,
-                 color=color,
-                 color_discrete_map=color_discrete_map)
+def update_traces(fig):      
+    fig.update_layout(hovermode='x unified')
+    fig.update_traces(mode="markers+lines", 
+                      hovertemplate="<b>%{y} pacientes<b>")  
     
-    update_layout(fig, title, x_title, y_title)
+def add_total_line(fig, df, x, y):
+    df_sum = df.groupby(x)[y].sum().reset_index(name=y)
+    fig.add_trace(go.Scatter(x=df_sum[x], 
+                             y=df_sum[y], 
+                             line=dict(color='#3B82F6'), 
+                             mode='lines', 
+                             name='Total'))
     
-    if(mean):
-        include_mean(fig, df, x, y, mean)
-    return fig
-
 
 def line_chart(df, x, y, title, x_title, y_title, color, mean=None):
     global color_discrete_map
@@ -57,8 +57,42 @@ def line_chart(df, x, y, title, x_title, y_title, color, mean=None):
                   markers=True,
                   color_discrete_map=color_discrete_map)
     
-    update_layout(fig, title, x_title, y_title)
+    add_total_line(fig=fig,
+                   df=df,
+                   x=x,
+                   y=y)
+    
+    update_traces(fig=fig)
+
+    update_layout(fig=fig, 
+                  title=title,
+                  x_title=x_title,
+                  y_title=y_title)
     
     if(mean):
-        include_mean(fig, df, x, y, mean)
+        include_mean(fig=fig,
+                     df=df,
+                     x=x,
+                     y=y)
+    return fig
+
+def bar_chart(df, x, y, title, x_title, y_title, color, mean=None):
+    global color_discrete_map
+    
+    fig = px.bar(df,
+                 x=x,
+                 y=y,
+                 color=color,
+                 color_discrete_map=color_discrete_map)
+    
+    update_layout(fig=fig,
+                  title=title,
+                  x_title=x_title,
+                  y_title=y_title)
+    
+    if(mean):
+        include_mean(fig=fig, 
+                     df=df,
+                     x=x, 
+                     y=y)
     return fig
