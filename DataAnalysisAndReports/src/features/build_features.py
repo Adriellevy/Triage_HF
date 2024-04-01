f976c1943ca8d010b63e1e05a4943e400385436c
Juan Manuel <juanmanuelgonzalezkapnik@gmail.com>
2024-04-01 11:00:00 -0300

    args first update (not enough) + args adapted to db + features building from db + get table added table join and on condition + date filters separated from chart building + filters adapted to db + metrics text fixes + visualize color map key fixed

--- DataAnalysisAndReports/src/features/build_features.py
+++ DataAnalysisAndReports/src/features/build_features.py
@@ -1,86 +1,77 @@
 import pandas as pd
-from datetime import timedelta
+import datetime as dt
 import data.make_dataset as md
 
-def filter_discharged(df):
-    return df[df['PATIENT_STATUS'] == 'ALTA']
+# def filter_isolated(df):
+#     return df[df['patient_isolated'] == True]
 
-def filter_isolated(df):
-    return df[df['PATIENT_STATUS'] == 'AISLAMIENTO']
+# def filter_box_type(df, box_type):
+#     df_box = md.get_table('BOX')
+#     new_df = df.merge(df_box, on='BOX_ID')
+#     return new_df[new_df['BOX_TYPE'] == box_type]
 
-def filter_box_type(df, box_type):
-    df_box = md.get_table('BOX')
-    new_df = df.merge(df_box, on='BOX_ID')
-    return new_df[new_df['BOX_TYPE'] == box_type]
+# def filter_doctor_username(df, doctor_username):
+#     df_user = md.get_table('USERS')
+#     new_df = df.merge(df_user, left_on='DOCTOR_ID', right_on='USER_ID')
+#     return new_df[new_df['USER_NAME'] == doctor_username]
 
-def filter_doctor_username(df, doctor_username):
-    df_user = md.get_table('USERS')
-    new_df = df.merge(df_user, left_on='DOCTOR_ID', right_on='USER_ID')
-    return new_df[new_df['USER_NAME'] == doctor_username]
+# def filter_nurse_username(df, nurse_username):
+#     df_user = md.get_table('USERS')
+#     new_df = df.merge(df_user, left_on='NURSE_ID', right_on='USER_ID')
+#     return new_df[new_df['USER_NAME'] == nurse_username]
 
-def filter_nurse_username(df, nurse_username):
-    df_user = md.get_table('USERS')
-    new_df = df.merge(df_user, left_on='NURSE_ID', right_on='USER_ID')
-    return new_df[new_df['USER_NAME'] == nurse_username]
+def date_filters(df, dictionary):
+    if ( dictionary['from_'] == None and dictionary['to'] == None):
+        dictionary['from_'], dictionary['to'] = get_last_week(df)
+    else:
+        dictionary['from_'] = pd.to_datetime(dictionary['from_'], format='%Y-%m-%d', errors='coerce')
+        dictionary['to'] = pd.to_datetime(dictionary['to'], format='%Y-%m-%d', errors='coerce')
+    
+    #print(from_)
+    
+    df = df[(df['patient_entry_time'] >= dictionary['from_']) & (df['patient_entry_time'] <= dictionary['to'])]
+    
+    return df
+
 
 def drop_id_feature(df):
     return df.iloc[:, 1:]
 
+def triage_level_style(df):
+    df['patient_triage_level'] = df['patient_triage_level'].apply(lambda x: str(int(float(x))))
+    df = df[df['patient_triage_level'] != '0']    
+    mapping = {'1': 'Nivel I', '2': 'Nivel II', '3': 'Nivel III', '4': 'Nivel IV'}
+    return df['patient_triage_level'].map(mapping)
+
 def get_last_week(df):
     to = df['patient_entry_time'].max()
-    from_ = to - timedelta(days=7)
+    from_ = to - dt.timedelta(days=6)
     return (from_, to)
 
 
-def filters(df, from_=None, to=None, patient_name=None, patient_symptom=None, box_type=None, doctor_username=None, nurse_username=None, discharged=None, isolated=None):
-    if (from_ == None and to == None):
-        from_, to = get_last_week(df)
+def filters(dictionary):
+    query = ''
+    for key, value in dictionary.items():
+        if value != None and key != 'patient_isolated' and key != 'from_' and key != 'to':
+            query += f'{key}=\'{value}\' AND '
+        elif key == 'patient_isolated' and value != None:
+            query += f'{key}={value} AND '
+     
+    print(dictionary)
+    
+    if query == '':
+        return None
     else:
-        from_ = pd.to_datetime(from_, format='%Y-%m-%d', errors='coerce')
-        to = pd.to_datetime(to, format='%Y-%m-%d', errors='coerce')
-
-    df = df[(df['patient_entry_time'] >= from_) & (df['patient_entry_time'] <= to)]
-
-    if (patient_name != None):
-        df = df[df['patient_name'] == patient_name.upper()]
-
-    if (patient_symptom != None):
-        df = df[df['patient_symptom'] == patient_symptom.upper()]
-
-    if (box_type != None):
-        df = filter_box_type(df, box_type)
-
-    if (doctor_username != None):
-        df = filter_doctor_username(df, box_type)
-
-    if (nurse_username != None):
-        df = filter_nurse_username(df, nurse_username)
-
-    if (discharged != None):
-        df = filter_discharged(df)
-
-    if (isolated != None):
-        df = filter_isolated(df)
-
-    return df
+        return query[:-5]
 
 def build_number_patients_date(df):    
     df['patient_entry_time'] = df['patient_entry_time'].dt.date
-    
     df = df.groupby(['patient_entry_time', 'patient_triage_level'], sort=False).size(
     ).reset_index(name='number_of_patients')
     
-
-    df['patient_triage_level'] = df['patient_triage_level'].apply(lambda x: str(int(float(x))))
-    df = df[df['patient_triage_level'] != '0']
-
     df.sort_values(by='patient_entry_time', inplace=True)
     
     df = df.sort_values(by=['patient_triage_level', 'patient_entry_time', 'number_of_patients'])
-    
-    mapping = {'1': 'Nivel I', '2': 'Nivel II', '3': 'Nivel III', '4': 'Nivel IV'}
-    
-    df['patient_triage_level'] = df['patient_triage_level'].map(mapping)
     
     return df
 
@@ -91,9 +82,6 @@ def build_top_queries_date(df, top=10, order=None):
     top_reasons = df.groupby('patient_symptom')[
         'symptom_count'].sum().nlargest(top).index
     count = df[df['patient_symptom'].isin(top_reasons)]
-
-    count['patient_triage_level'] = count['patient_triage_level'].apply(lambda x: str(int(float(x))))
-    count = count[count['patient_triage_level'] != '0']
     
     if (order == 'asc'):
         count = count.sort_values(by=['patient_symptom', 'patient_triage_level'], key=lambda x: x.map(
@@ -101,18 +89,22 @@ def build_top_queries_date(df, top=10, order=None):
 
     count = count.sort_values(by=['patient_symptom', 'patient_triage_level'], key=lambda x: x.map(
         dict(zip(top_reasons[::-1], range(len(top_reasons)))))).reset_index(drop=True)
-    
-    mapping = {'1': 'Nivel I', '2': 'Nivel II', '3': 'Nivel III', '4': 'Nivel IV'}
-    
-    df['patient_triage_level'] = df['patient_triage_level'].map(mapping)
 
     return count
 
-def build_features(df, from_=None, to=None, patient_name=None, patient_symptom=None, box_type=None, doctor_username=None, nurse_username=None, discharged=None, isolated=None):
+def build_features(table_name, dictionary):
+    
+    query = filters(dictionary)
+    
+    df = md.get_table(table_name, query)
+    
+    if df.empty:
+        return df
+    
+    df = date_filters(df, dictionary)
+
     df = drop_id_feature(df)
     
-    df = filters(df, from_, to, patient_name, patient_symptom,
-                 box_type, doctor_username, nurse_username, discharged, isolated)
-    
+    df['patient_triage_level'] = triage_level_style(df)
     
     return df
--- DataAnalysisAndReports/src/visualization/metrics.py
+++ DataAnalysisAndReports/src/visualization/metrics.py
@@ -1,18 +1,4 @@
 import statistics as st
-# 
-# 
-# IDEAS METRICAS
-#
-#
-
-#
-# TENGO QUE
-#
-#   
-# checkear metricas para cada nivel de triage
-#
-#
-#
 
 def calculate_total(df, y):
     return df[y].sum()
@@ -39,13 +25,13 @@ def total(txt_y, df, y):
     return f'El total de {txt_y} ingresados es de {calculate_total(df, y)}'
 
 def mean(txt_y, df, y):
-    return f'La media de {txt_y} es de {round(calculate_mean(df, y),2)}'
+    return f'La media de {txt_y} ingreasdos es de {round(calculate_mean(df, y),2)}'
 
 def min(txt_x, txt_y, df, x, y):
-    return f'La {txt_x} que menos {txt_y} se registraron fue el {calculate_min_x(df, x, y)} con {calculate_min(df, y)} {txt_y}'
+    return f'La {txt_x} que menos {txt_y} se ingresaron fue el {calculate_min_x(df, x, y)} con {calculate_min(df, y)} {txt_y}'
 
 def max(txt_x, txt_y, df, x, y):
-    return f'La {txt_x} que más {txt_y} se registraron fue el {calculate_max_x(df, x, y)} con {calculate_max(df, y)} {txt_y}'
+    return f'La {txt_x} que más {txt_y} se ingresaron fue el {calculate_max_x(df, x, y)} con {calculate_max(df, y)} {txt_y}'
 
 def metrics_data(df, x, y, txt_x, txt_y):
     df_sum = df.groupby(df[x])[y].sum().reset_index(name='number_of_patients')
