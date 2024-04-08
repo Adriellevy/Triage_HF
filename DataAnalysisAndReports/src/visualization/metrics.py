0542600cab2b3533db21c27cbc12e1fecdb2ad6f
Juan Manuel <juanmanuelgonzalezkapnik@gmail.com>
2024-04-08 07:24:13 -0300

    metrics update + patient mean time doctor chart started

--- DataAnalysisAndReports/src/visualization/metrics.py
+++ DataAnalysisAndReports/src/visualization/metrics.py
@@ -1,70 +1,77 @@
-def calculate_total(df, y):
+import pandas as pd
+from typing import Any, Dict
+
+def calculate_total(df: pd.DataFrame, y: str) -> int:
     return df[y].sum()
 
-def calculate_mean(df, y):
+def calculate_mean(df: pd.DataFrame, y: str) -> float:
     return df[y].mean()
-    
 
-def calculate_median(df, y):
-    return df[y].median()
-
-def calculate_max(df, y):
+def calculate_max(df: pd.DataFrame, y: str) -> Any:
     return df[y].max()
 
-def calculate_min(df, y):
+def calculate_min(df: pd.DataFrame, y: str) -> Any:
     return df[y].min()
 
-def calculate_min_x(df, x, y):
+def calculate_min_x(df: pd.DataFrame, y: str, x: str) -> Any:
     return df.loc[df[y].idxmin(), x]
 
-def calculate_max_x(df, x, y):
+def calculate_max_x(df: pd.DataFrame, y: str, x: str) -> Any:
     return df.loc[df[y].idxmax(), x]
 
-def total(txt, df_sum, y):
-    return f'El total de {txt} fue de {calculate_total(df_sum, y)}'
+def total(df: pd.DataFrame, y: str, txt: str, z: str = '', lvl: str = '') -> str:
+    string = f'El total de {txt} '
+    if z != '' and lvl != '':
+        df = df[df[z] == lvl]
+        string += f'en {lvl} '
+    string += f'fue de {calculate_total(df, y)}'
+    return string
 
-def total_triage_I(txt, df, y, z):
-    df = df[df[z] == 'Nivel I']
-    return f'El total de {txt} de Nivel I fue de {calculate_total(df, y)}'
+def mean(df: pd.DataFrame, y: str, txt: str, z: str = '', lvl: str = '') -> str:
+    string = f'La media de {txt} '
+    if z != '' and lvl != '':
+        df = df[df[z] == lvl]
+        string += f'en {lvl} '
+    string += f'fue de {round(calculate_mean(df, y), 2)}'
+    return string
+    
+def min(df: pd.DataFrame, y: str, txt: str, x:str, z: str = '', lvl: str = '') -> str:
+    string = f'El minimo de {txt} '
+    if z != '' and lvl != '':
+        df = df[df[z] == lvl]
+        string += f'en {lvl} '
+    string += f'fue de {calculate_min(df, y)} ({calculate_min_x(df, y, x)})'
+    return string
 
-def total_triage_II(txt, df, y, z):
-    df = df[df[z] == 'Nivel II']
-    return f'El total de {txt} de Nivel II fue de {calculate_total(df, y)}'
+def max(df: pd.DataFrame, y: str, txt: str, x:str, z: str = '', lvl: str = '') -> str:
+    string = f'El maximo de {txt} '
+    if z != '' and lvl != '':
+        df = df[df[z] == lvl]
+        string += f'en {lvl} '
+    string += f'fue de {calculate_max(df, y)} ({calculate_max_x(df, y, x)})'
+    return string
 
-def total_triage_III(txt, df, y, z):
-    df = df[df[z] == 'Nivel III']
-    return f'El total de {txt} de Nivel III fue de {calculate_total(df, y)}'
-
-def total_triage_IV(txt, df, y, z):
-    df = df[df[z] == 'Nivel IV']
-    return f'El total de {txt} de Nivel IV fue de {calculate_total(df, y)}'
-
-def mean(txt, df_sum, y):
-    return f'La media de {txt} fue de {round(calculate_mean(df_sum, y), 2)}'
-
-def mean_triage_I(txt, df, y, z):
-    df = df[df[z] == 'Nivel I']
-    return f'La media de {txt} de Nivel I fue de {round(calculate_mean(df, y), 2)}'    
-
-def min(txt, df_sum, x, y):
-    return f'El minimo de {txt} fue de {calculate_min(df_sum, y)} en la fecha {calculate_min_x(df_sum, x, y)}'
-
-def max(txt, df_sum, x, y):
-    return f'El maximo de {txt} fue de {calculate_max(df_sum, y)} en la fecha {calculate_max_x(df_sum, x, y)}'
-
-def metrics_data(txt, df, x, y, z):
-    df_sum = df.groupby(df[x])[y].sum().reset_index(name=y)
+def metrics_data(df: pd.DataFrame, y: str, txt: str, x:str, z: str) -> Dict[str, str]:
+    df_sum: pd.DataFrame = df.groupby(df[x])[y].sum().reset_index(name=y)
     return {
-        'total': total(txt, df_sum, y),
-        'total_triage_I': total_triage_I(txt, df, y, z),
-        'total_triage_II': total_triage_II(txt, df, y, z),
-        'total_triage_III': total_triage_III(txt, df, y, z),
-        'total_triage IV': total_triage_IV(txt, df, y, z),
-        'mean': mean(txt, df_sum, y),
-        'mean_triage_I': mean_triage_I(txt, df, y, z),
-        # 'mean_triage_II': mean_triage_II(),
-        # 'mean_triage_III': mean_triage_III(),
-        # 'mean_triage_IV': mean_triage_IV(),
-        'min': min(txt, df_sum, x, y),
-        'max': max(txt, df_sum, x, y)
+        'total': total(df, y, txt),
+        'total_triage_I': total(df, y, txt, z, 'Nivel I'),
+        'total_triage_II': total(df, y, txt, z, 'Nivel II'),
+        'total_triage_III': total(df, y, txt, z, 'Nivel III'),
+        'total_triage IV': total(df, y, txt, z, 'Nivel IV'),
+        'mean': mean(df_sum, y, txt),
+        'mean_triage_I': mean(df, y, txt, z, 'Nivel I'),
+        'mean_triage_II': mean(df, y, txt, z, 'Nivel II'),
+        'mean_triage_III': mean(df, y, txt, z, 'Nivel III'),
+        'mean_triage_IV': mean(df, y, txt, z, 'Nivel IV'),
+        'min': min(df_sum, y, txt, x),
+        'min_triage_I': min(df, y, txt, x, z, 'Nivel I'),
+        'min_triage_II': min(df, y, txt, x, z, 'Nivel II'),
+        'min_triage_III': min(df, y, txt, x, z, 'Nivel III'),
+        'min_triage_IV': min(df, y, txt, x, z, 'Nivel IV'),
+        'max': max(df_sum, y, txt, x),
+        'max_triage_I': max(df, y, txt, x, z, 'Nivel I'),
+        'max_triage_II': max(df, y, txt, x, z, 'Nivel II'),
+        'max_triage_III': max(df, y, txt, x, z, 'Nivel III'),
+        'max_triage_IV': max(df, y, txt, x, z, 'Nivel IV')
     }
