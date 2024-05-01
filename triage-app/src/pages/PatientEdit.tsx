a8250e154f25e0189f220feb02e68b0681dd4c84
adriel levy <adriellevy2014@hotmail.com>
2024-05-01 20:59:19 -0300

    Diferenciacion de formularios por rol

--- triage-app/src/pages/PatientEdit.tsx
+++ triage-app/src/pages/PatientEdit.tsx
@@ -1,4 +1,5 @@
 import PatientForm from '@/components/Forms/PatientForm'
+import PatientFormDoctor from '@/components/Forms/PatientFormDoctor'
 import PatientHistory from '@/components/PatientHistory/PatientHistory'
 import { UserRole } from '@/interfaces/User'
 import { useParams } from 'react-router-dom'
@@ -7,8 +8,8 @@ function PatientEdit({ user_tipe }: { user_tipe: UserRole }) {
   const { edditingPatientID } = useParams()
   return (
     <div className='mb-4'>
-      {user_tipe === UserRole.NURSE && <PatientForm />}
-      {user_tipe === UserRole.DOCTOR && <PatientForm />}
+      {user_tipe === UserRole.DOCTOR && <PatientFormDoctor />}
+      {user_tipe != UserRole.DOCTOR && <PatientForm />}
       <div className='max-w-6xl mx-auto mt-5 p-6 bg-white shadow-md rounded-md'>
         <PatientHistory patient_id={edditingPatientID} />
       </div>
