b02dfee5de01afa012898fb1d2645f039fdf5826
Lucas <lucasgomezoliva@gmail.com>
2024-05-23 19:57:31 -0300

    Add notification helper

---
+++ triage-app-server/src/helpers/notificationhelper.ts
@@ -0,0 +1,86 @@
+import { type Request } from 'express';
+import { type Patient } from '../interface/patient';
+
+enum SocketEvent {
+  GLOBAL_NOTIFICATION = 'notification',
+  UPDATE = 'update'
+}
+
+enum UpdateEvent {
+  NEW_PATIENT = 'New patient',
+  NEW_PATIENT_ASSIGNED = 'New patient assigned',
+  BOX_UPDATE = 'Box Update',
+  UPDATE_PATIENT = 'Updated patient'
+}
+
+export function SendNewPatientNotifications(
+  req: Request,
+  newpatient: Patient,
+  userID: string
+): void {
+  const io = req.io;
+
+  io?.emit(SocketEvent.UPDATE, {
+    message: UpdateEvent.NEW_PATIENT
+  });
+
+  io?.emit(SocketEvent.UPDATE, {
+    message: UpdateEvent.BOX_UPDATE
+  });
+
+  if (userID !== newpatient.doctor_id) {
+    io?.emit(`${newpatient.doctor_id}`, {
+      message: UpdateEvent.NEW_PATIENT_ASSIGNED,
+      patient: {
+        patient_name: newpatient.patient_name,
+        patient_id: newpatient.patient_id
+      }
+    });
+  }
+
+  if (userID !== newpatient.nurse_id) {
+    io?.emit(`${newpatient.nurse_id}`, {
+      message: UpdateEvent.NEW_PATIENT_ASSIGNED,
+      patient: {
+        patient_name: newpatient.patient_name,
+        patient_id: newpatient.patient_id
+      }
+    });
+  }
+}
+
+export function SendUpdatePatientNotifications(
+  req: Request,
+  UpdatedPatient: Patient,
+  userID: string
+): void {
+  const io = req.io;
+
+  io?.emit(SocketEvent.UPDATE, {
+    message: UpdateEvent.NEW_PATIENT
+  });
+
+  io?.emit(SocketEvent.UPDATE, {
+    message: UpdateEvent.BOX_UPDATE
+  });
+
+  if (userID !== UpdatedPatient.doctor_id) {
+    io?.emit(`${UpdatedPatient.doctor_id}`, {
+      message: UpdateEvent.NEW_PATIENT_ASSIGNED,
+      patient: {
+        patient_name: UpdatedPatient.patient_name,
+        patient_id: UpdatedPatient.patient_id
+      }
+    });
+  }
+
+  if (userID !== UpdatedPatient.nurse_id) {
+    io?.emit(`${UpdatedPatient.nurse_id}`, {
+      message: UpdateEvent.NEW_PATIENT_ASSIGNED,
+      patient: {
+        patient_name: UpdatedPatient.patient_name,
+        patient_id: UpdatedPatient.patient_id
+      }
+    });
+  }
+}
