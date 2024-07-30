5e063d0b87990124cb1276eb2d496f38b0153a13
Shalom4All <adriellevy2014@gmail.com>
2024-07-30 19:18:32 -0300

    Warning box + ConflictResolver implementation

--- triage-app/src/interfaces/ConflictResolver.ts
+++ triage-app/src/interfaces/ConflictResolver.ts
@@ -1,18 +1,17 @@
-import {  PatientData } from "./Patinet";
+import { PatientData } from './Patinet'
 
 interface ConflictData {
-  currentData: Record<string, string[] | null>;
-  newData: Record<string, string[] | null>;
+  currentData: Record<string, string[] | null>
+  newData: Record<string, string[] | null>
 }
-  
-  export interface ConflictResolverProps {
-    conflictData: ConflictData;
-    onResolve: (mergedData: Record<string, string>) => void;
-    onAcceptCurrent: () => void;
-    onCancel: () => void;
-  }
-  export interface Field {
-    label: string
-    key: keyof PatientData
-    format: ((value: string) => string | Promise<string | null>) | null
-  }
+
+export interface ConflictResolverProps {
+  conflictData: ConflictData
+  onResolve: (mergedData: Record<string, string>) => void
+  onCancel: () => void
+}
+export interface Field {
+  label: string
+  key: keyof PatientData
+  format: ((value: string) => string | Promise<string | null>) | null
+}
