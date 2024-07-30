3e521fc3ba26dd9e4633366e82604214395c395f
Shalom4All <adriellevy2014@gmail.com>
2024-07-30 09:51:30 -0300

    Fechas conflict Resolver

--- triage-app/src/helpers/HelperFechas.ts
+++ triage-app/src/helpers/HelperFechas.ts
@@ -1,6 +1,6 @@
-// helper.ts
 export const getFormatBirthDate = (birth__date: string): string => {
   const originalBirthDate = new Date(birth__date);
+  const utcBirthDate = new Date(originalBirthDate.getTime() + originalBirthDate.getTimezoneOffset() * 60000);
   const birthFormat: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
@@ -8,12 +8,13 @@ export const getFormatBirthDate = (birth__date: string): string => {
     hour12: true
   };
   const formatoNacimiento = new Intl.DateTimeFormat('es-AR', birthFormat);
-  const formatBirthDate = formatoNacimiento.format(originalBirthDate);
+  const formatBirthDate = formatoNacimiento.format(utcBirthDate);
   return formatBirthDate;
 };
 
 export const getFormatDate = (entry__time: string): string => {
   const entryTimeOriginal = new Date(entry__time);
+  const utcEntryTime = new Date(entryTimeOriginal.getTime() + entryTimeOriginal.getTimezoneOffset() * 60000);
   const dateFormat: Intl.DateTimeFormatOptions = {
     year: 'numeric',
     month: 'long',
@@ -23,15 +24,15 @@ export const getFormatDate = (entry__time: string): string => {
     hour12: true
   };
   const formatoFechaHora = new Intl.DateTimeFormat('es-AR', dateFormat);
-  const formatEntryTime = formatoFechaHora.format(entryTimeOriginal);
+  const formatEntryTime = formatoFechaHora.format(utcEntryTime);
   return formatEntryTime;
 };
 
 export const getAgeNumber = (edad: number) => {
   if (edad) {
-    const hoy = new Date()
-    const añoActual = hoy.getFullYear()
-    const añoNacimiento = añoActual - edad
-    return new Date(añoNacimiento + '-01-01')
+    const hoy = new Date();
+    const añoActual = hoy.getFullYear();
+    const añoNacimiento = añoActual - edad;
+    return new Date(añoNacimiento + '-01-01');
   }
-}
+};
