61ae1c8f7f93aebdf38781fec68605db8e878bd6
adriel levy <adriellevy2014@hotmail.com>
2024-08-20 18:48:37 -0300

    BoxSettings page

--- triage-app/src/pages/settings/BoxSettings.tsx
+++ triage-app/src/pages/settings/BoxSettings.tsx
@@ -1,9 +1,39 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { SocketContext } from '@/contex/SocketContext';
+import { getAllBoxes } from '@/services/boxService';
+import { Box } from '@/interfaces/Boxes';
+import { useTranslation } from 'react-i18next';
+import LoaderSpin from '@/components/LoaderSpin';
+import BoxEdittingList from '../../components/BoxSettingsComponents/BoxEdittingList';
+
 function BoxSettings() {
+  const { t } = useTranslation('Boxes');
+  const [isLoading, setIsLoading] = useState(false);
+  const [boxesData, setboxesData] = useState<Box[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setIsLoading(true);
+        const data = await getAllBoxes();
+        setboxesData(data);
+        setIsLoading(false);
+      } catch (error) {
+        console.error((error as Error).message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <div>
-      <h1>Box settings</h1>
+    <div className="bg-white pb-4">
+      {isLoading ? (
+        <LoaderSpin/>
+      ) : (
+        <BoxEdittingList initialBoxes={boxesData} />
+      )}
     </div>
-  )
+  );
 }
 
-export default BoxSettings
+export default BoxSettings;
