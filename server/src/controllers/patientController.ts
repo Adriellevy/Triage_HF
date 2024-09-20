ef0654da3e87a4d4bf965add448a52f424ef46f9
Adriel <adriellevy2014@gmail.com>
2024-09-20 10:10:28 -0300

    mismo

--- server/src/controllers/patientController.ts
+++ server/src/controllers/patientController.ts
@@ -249,6 +249,7 @@ export class PatientController {
 
       // Validar que las fechas sean cadenas de texto
       if (typeof StartDate !== 'string' || typeof EndDate !== 'string') {
+        console.log('No pasaron las strings');
         return res.status(400).json({ message: 'StartDate y EndDate deben ser cadenas de texto' });
       }
 
