9e9e9d05f1b63aa0f329e8e1b5997e78205b2525
junam-py <juanmanuelgonzalezkapnik@gmail.com>
2024-10-06 19:28:56 -0300

    make dataset changes

--- DataAnalysisAndReports/src/data/make_dataset.py
+++ DataAnalysisAndReports/src/data/make_dataset.py
@@ -1,43 +1,53 @@
 import os
-import typing
-
-import mysql.connector
+from typing import Any, Dict, Union
+import requests
 import pandas as pd
 from dotenv import load_dotenv
-from mysql.connector.connection import MySQLConnection
-from mysql.connector.connection_cext import CMySQLConnection
+# import mysql.connector
+# from mysql.connector.connection import MySQLConnection
+# from mysql.connector.connection_cext import CMySQLConnection
 
-load_dotenv()
+load_dotenv('.env')
 
 
-def connect() -> typing.Union[MySQLConnection, CMySQLConnection, None]:
+# def connect() -> Union[MySQLConnection, CMySQLConnection, None]:
+#     try:
+#         connection = mysql.connector.connect(
+#             host=os.getenv('DB_HOST'),
+#             user=os.getenv('DB_USER'),
+#             password=os.getenv('DB_PASSWORD'),
+#             port=os.getenv('DB_PORT'),
+#             database=os.getenv('DB_NAME'),
+#             charset=os.getenv('DB_CHARSET')
+#         )
+#     except mysql.connector.Error:
+#         return None
+#     return connection
+
+def fetch_data_from_api(endpoint: str, params: Dict[str, Any] = None, headers: Dict[str, str] = None) -> Union[pd.DataFrame, None]:
+    python_server_token = os.getenv('PYTHONSERVER')
+
+    if headers is None:
+        headers = {}
+    headers['Authorization'] = f'Bearer {python_server_token}'
+
     try:
-        connection = mysql.connector.connect(
-            host=os.getenv('DB_HOST'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            port=os.getenv('DB_PORT'),
-            database=os.getenv('DB_NAME'),
-            charset=os.getenv('DB_CHARSET')
-        )
-    except mysql.connector.Error:
+        response = requests.get(endpoint, params=params, headers=headers)
+        response.raise_for_status()
+        data = response.json()
+        return pd.DataFrame(data)
+    except requests.RequestException as e:
+        print(f"HTTP Request failed: {e}")
         return None
-    return connection
 
-
-def get_table(table_name: str, condition: str = '') -> typing.Union[pd.DataFrame, None]:
-    connection = connect()
-    if connection:
-        query = 'SELECT * FROM ' + table_name
-        if condition != '':
-            query += condition
-        print('QUERY:   \"' + query + '\"')
-        df = pd.read_sql(sql=query,
-                         con=connection)
-        connection.close()
-        return df
-    else:
-        return None
+def get_table(table_name: str, condition: str = '') -> Union[pd.DataFrame, None]:
+    query = 'SELECT * FROM ' + table_name
+    if condition != '':
+        query += condition
+    print('QUERY:   \"' + query + '\"')
+    endpoint='http://localhost:3000'
+    df = fetch_data_from_api(endpoint, params=query, headers=None)
+    return df
 
 # def get_id(table_name):
 #     connection = connect()
