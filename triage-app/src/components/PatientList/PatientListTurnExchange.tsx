62cb167df2de4a8607a766eb95016c0c5933ea0a
Facundo Barberia <facundobarberia@gmail.com>
2024-11-19 20:11:51 -0300

    TurnExchange-Front

---
+++ triage-app/src/components/PatientList/PatientListTurnExchange.tsx
@@ -0,0 +1,152 @@
+import { useState } from 'react'
+import { Patient } from '@/interfaces/Patinet'
+import { useTranslation } from 'react-i18next'
+import Pagination from '../Pagination/Pagination'
+import PatientItemTurnExchange from './PatientItemTurnExchange'
+
+interface PropsPatientsList {
+  patients: Patient[]
+  mode: string
+  setNewDoctor?: (name: string) => void 
+  setNewNurse?: (name: string) => void
+}
+
+function PatientsListTurnExchange({ patients, mode, setNewDoctor, setNewNurse }: PropsPatientsList) {
+  const { t } = useTranslation('PatientList')
+  const [currentPage, setCurrentPage] = useState(1);
+  const [sortColumn, setSortColumn] = useState<string | null>(null)
+  const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
+  const patientsPerPage = 5
+
+  const columns = [
+    { label: t('NameLabel'), 
+        field: 'patient_name', 
+        sortable: true, 
+        showOnLargeScreen: true,
+        mode: 'all'
+    },
+    {
+      label: t('TriageLevelLabel'),
+      field: 'patient_triage_level',
+      sortable: true,
+      showOnLargeScreen: true,
+      mode: 'all'
+    },
+    {
+      label: t('LastDoctorNameLabel'),
+      field: 'doctor_name',
+      sortable: true,
+      showOnLargeScreen: false,
+      mode: 'doctor'
+    },
+    {
+        label: t('NewDoctorNameLabel'),
+        field: 'doctor_name',
+        sortable: false,
+        showOnLargeScreen: false,
+        mode: 'doctor'
+      },
+    {
+      label: t('LastNurseNameLabel'),
+      field: 'nurse_name',
+      sortable: true,
+      showOnLargeScreen: false,
+      mode: 'nurse'
+    },
+    {
+        label: t('NewNurseNameLabel'),
+        field: 'nurse_name',
+        sortable: false,
+        showOnLargeScreen: false,
+        mode: 'nurse'
+      },
+    {
+      label: t('PatientStatusLabel'),
+      field: 'patient_status',
+      sortable: true,
+      showOnLargeScreen: true,
+      mode: 'all'
+    },
+    { label: t('PatientActionsLabel'), 
+        field: 'actions',
+        sortable: false,
+        showOnLargeScreen: true,
+        mode: 'all'
+     }
+  ]
+
+  const handleSort = (column: string) => {
+    if (sortColumn === column) {
+      setSortDirection(sortDirection === 'asc' ? 'desc' : 'asc')
+    } else {
+      setSortColumn(column)
+      setSortDirection('asc')
+    }
+  }
+
+  const handlePagination = (pageNumber: number) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const sortedPatients = [...patients].sort((a, b) => {
+    if (!sortColumn) {
+      return 0
+    }
+    const columnA = a[sortColumn as keyof Patient]
+    const columnB = b[sortColumn as keyof Patient]
+    if (typeof columnA === 'string' && typeof columnB === 'string') {
+      return sortDirection === 'asc'
+        ? columnA.localeCompare(columnB)
+        : columnB.localeCompare(columnA)
+    } else if (typeof columnA === 'number' && typeof columnB === 'number') {
+      return sortDirection === 'asc' ? columnA - columnB : columnB - columnA
+    } else {
+      return 0
+    }
+  })
+
+  const indexOfLastPatient = currentPage * patientsPerPage;
+  const indexOfFirstPatient = indexOfLastPatient - patientsPerPage;
+  const currentPatients = sortedPatients.slice(indexOfFirstPatient, indexOfLastPatient);
+  return (
+    <>
+        <div className='text-xs md:text-sm mt-4 lg:mx-8'>
+        <table className='w-full border border-gray-300'>
+          <thead>
+          <tr className="bg-blue-800 text-white">
+  {columns
+    .filter((column) => column.mode ===  'all' || column.mode === mode) // Filtrar columnas según el mode
+    .map((column) => (
+      <th
+        key={column.field}
+        className="border cursor-pointer p-2"
+        onClick={() => (column.sortable ? handleSort(column.field) : null)}
+      >
+        {column.label}{' '}
+        {column.sortable && sortColumn === column.field && (
+          <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
+        )}
+      </th>
+    ))}
+            </tr>
+          </thead>
+          <tbody>
+            {currentPatients.map((patient, index) => (
+              <PatientItemTurnExchange key={patient.patient_id} patient={patient} index={index} mode={mode} setNewDoctor={setNewDoctor} setNewNurse={setNewNurse} />
+            ))}
+          </tbody>
+        </table>
+        <div className='flex justify-center'>
+          <Pagination
+            patientsPerPage={patientsPerPage}
+            length={patients.length}
+            currentPage={currentPage}
+            onPageChange={handlePagination}
+          />
+        </div>
+      </div>
+    </>
+  );
+}  
+
+export default PatientsListTurnExchange
--- triage-app/src/components/TurnExchangeModal.tsx
+++ triage-app/src/components/TurnExchangeModal.tsx
@@ -1,24 +1,24 @@
-import { useEffect, useState } from "react";
+import { SetStateAction, useEffect, useState } from "react";
 import  Select  from "react-select";
 import { Button } from "./ui";
 import { getAllDoctors, getAllNurses } from "@/services/userService";
-import { getPatientsByDr, getPatientsByUserID } from "@/services/patientService";
+import {  getPatientsByUserID } from "@/services/patientService";
 import { Patient } from "@/interfaces/Patinet";
-import PatientsList from "./PatientList/PatientsList";
+import PatientsListTurnExchange from "./PatientList/PatientListTurnExchange";
+import { Checkbox } from "@mui/material";
 
 const TurnExchangeModal = ({ onClose }) => {
     const [lastDoctor, setLastDoctor] = useState('')
-    const [newDoctor, setNewDoctor] = useState('')
-    const [newDoctorName, setNewDoctorName] = useState('')
     const [lastNurse, setLastNurse] = useState('')
+    const [newDoctor, setNewDoctor] = useState('')
     const [newNurse, setNewNurse] = useState('')
-    const [newNurseName, setNewNurseName] = useState('')
     const [doctorOptions, setDoctorOptions] = useState<ColourOption[]>([])
     const [nurseOptions, setNurseOptions] = useState<ColourOption[]>([])
-    const [patients, setPatients] = useState<Patient[]>([])
-    const [currentPage, setCurrentPage] = useState(1)
+    const [doctorPatients, setDoctorPatients] = useState<Patient[]>([])
+    const [nursePatients, setNursePatients] = useState<Patient[]>([])
+    const [createReport, setCreateReport] = useState(false)
 
-    const handleSelection = (selectedOption, slot, role) => {
+    const handleSelection = (selectedOption: { value: SetStateAction<string>; label: any; }, slot: string, role: string) => {
       if (selectedOption) {
         if (role === 'doctor') {
           if (slot === 'last') {
@@ -66,35 +66,49 @@ const TurnExchangeModal = ({ onClose }) => {
       useEffect(() => {
         const fetchPatients = async () => {
           try {
-            let oldpatients = [];
+            setDoctorPatients([])
+            let oldpatients: any[] | ((prevState: Patient[]) => Patient[]) = []
             if (lastDoctor) {
               const doctorPatients = await getPatientsByUserID(lastDoctor);
               oldpatients = oldpatients.concat(doctorPatients); // Use concat or spread
             }
-            
-            if (lastNurse) {
-              const nursePatients = await getPatientsByUserID(lastNurse);
-              oldpatients = oldpatients.concat(nursePatients); // Use concat or spread
-            }
-            setPatients(oldpatients)
+            setDoctorPatients(oldpatients)
           } catch (error) {
             console.error("Error fetching patients:", error);
           }
         };
       
         fetchPatients();
-      }, [lastDoctor, lastNurse]);
+      }, [lastDoctor]);
+
+      useEffect(() => {
+        const fetchPatients = async () => {
+          try {
+            setNursePatients([])
+            let oldpatients: any[] | ((prevState: Patient[]) => Patient[]) = [];
+            if (lastNurse) {
+              const nursePatients = await getPatientsByUserID(lastNurse);
+              oldpatients = oldpatients.concat(nursePatients); // Use concat or spread
+            }
+            setNursePatients(oldpatients)
+          } catch (error) {
+            console.error("Error fetching patients:", error);
+          }
+        };
+      
+        fetchPatients();
+      }, [lastNurse]);
 
     return (
       <div className='fixed top-0  left-0 h-full w-full flex items-start justify-center bg-black bg-opacity-35 z-50 '>
-        <div className=" flex  flex-col bg-blue-900 rounded-lg mt-10 ml-56  max-w-2xl md:max-w-6xl w-full z-60">
+        <div className=" flex  flex-col bg-blue-900 rounded-lg mt-10 ml-56  max-w-2xl md:max-w-screen-2xl w-full z-60">
          <div className="flex mx-2 my-4 justify-between">
             <h3 className="text-2xl font-bold mx-2 my-2 mr-20">Cambio de turno</h3>
             <Button color='grey' onClick={onClose} className="text-sm py-0 px-2 font-bold ">
                   Cancelar
             </Button>
         </div>   
-        <div className='bg-white w-full rounded-lg p-8'>
+        <div className='bg-white w-full rounded-lg p-4'>
         <div className="flex">
           {/* LAST WORKERS */}
         <div className="flex-col w-full">
@@ -103,7 +117,7 @@ const TurnExchangeModal = ({ onClose }) => {
             <Select
             className='w-full text-black'
             options={doctorOptions}
-            placeholder={'Selecionar doctor/a'}
+            placeholder={'Selec. doctor/a'}
             closeMenuOnSelect={true}
             onChange={(selectedOption) => handleSelection(selectedOption, 'last', 'doctor')}
             />
@@ -115,55 +129,39 @@ const TurnExchangeModal = ({ onClose }) => {
             <Select
             className='w-full text-black'
             options={nurseOptions}
-            placeholder={'Selecionar enfermero/a'}
+            placeholder={'Selec. enfermero/a'}
             closeMenuOnSelect={true}
             onChange={(selectedOption) => handleSelection(selectedOption, 'last', 'nurse')}
             />
         </div>
         </div>
     </div>
-    <div className="flex mt-4">
-      {/* NEW WORKERS */}
-        <div className="flex-col w-full">
-        <label className='text-sm font-medium text-gray-700 mb-2 px-4'>Doctor/a entra:</label>
-        <div className='bg-white flex px-4'>
-            <Select
-            className='w-full text-black'
-            options={doctorOptions}
-            placeholder={'Selecionar doctor/a'}
-            closeMenuOnSelect={true}
-            onChange={(selectedOption) => handleSelection(selectedOption, 'new', 'doctor')}
-            />
-        </div>
-        </div>
-        <div className="flex-col w-full">
-        <label className='text-sm font-medium text-gray-700 mb-2 px-4'>Enfermero/a que entra:</label>
-        <div className='bg-white flex px-4'>
-            <Select
-            className='w-full text-black'
-            options={nurseOptions}
-            placeholder={'Selecionar enfermero/a'}
-            closeMenuOnSelect={true}
-            onChange={(selectedOption) => handleSelection(selectedOption, 'new', 'nurse')}
-            />
-        </div>
-        </div>
-    </div>
     {/* BUTTON */}
     <div className="flex justify-end mt-4">
+    <label className="text-black self-center ">Generar informe</label>
+    <Checkbox
+      name="Generar informe"
+      color="success"
+      checked={createReport}
+      onChange={() => setCreateReport(!createReport)}
+    />
     <Button color='green' onClick={onClose} className="text-lg py-2 px-2 font-bold ">
                   Cambio de turno
     </Button>
     </div>
     <div className="text-black flex justify-center">
     { lastDoctor ?
-      <PatientsList 
-      patients={patients}
-      currentPage={currentPage}
-      setCurrentPage={setCurrentPage}
-      turnExchange={true}
-      newDoctor={newDoctorName}
-      newNurse={newNurseName}
+      <PatientsListTurnExchange
+      patients={doctorPatients}
+      mode={'doctor'}
+      setNewDoctor={setNewDoctor}
+      />
+      : null}
+      { lastNurse ?
+      <PatientsListTurnExchange
+      patients={nursePatients}
+      mode={'nurse'}
+      setNewNurse={setNewNurse}
       />
       : null}
       </div>
