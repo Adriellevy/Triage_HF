54a466ae1f5d8642ce7d963ae7a40cb7b73ee087
Adriel <adriellevy2014@gmail.com>
2025-02-13 15:27:18 -0300

    Mejor estilo si no hay cambio de turnos

--- triage-app/src/pages/ShiftChange.tsx
+++ triage-app/src/pages/ShiftChange.tsx
@@ -5,11 +5,15 @@ import { getAllShifts } from '../services/ShiftService'
 import type { ShiftChange } from '@/interfaces/Shift-change.ts'
 import ShiftLists from '@/components/ShiftChangeList/ShiftLists.tsx'
 import { Shift } from '@/interfaces/Shift'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronRight } from '@fortawesome/free-solid-svg-icons'
+import { useTranslation } from 'react-i18next'
 
 function ShiftChange() {
   const token = Cookies.get('authToken')
   const [shiftsData, setShiftsData] = useState<Shift[] | null>(null)
 
+  const { t } = useTranslation('ShiftList')
   useEffect(() => {
     const fetchData = async () => {
       try {
@@ -31,7 +35,10 @@ function ShiftChange() {
       {/*><ShiftChangeList shiftChanges={shiftsData} /> */}
     </>
   ) : (
-    <p>No shifts found.</p>
+    <div className='flex flex-col items-center justify-center h-full'>
+      <FontAwesomeIcon icon={faChevronRight} className='text-6xl text-gray-400 mb-4' />
+      <p className='text-xl text-gray-500'>{t('noshift')}</p>
+    </div>
   )
 }
 export default ShiftChange
