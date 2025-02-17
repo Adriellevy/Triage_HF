a079fc09fcb38c9916f269f24ed112de021c2905
Adriel <adriellevy2014@gmail.com>
2025-02-17 18:39:25 -0300

    Parametrizacion de los listados de  los cambios de turnos a desplegar

--- triage-app/src/components/ShiftChangeList/ShiftChangeList.tsx
+++ triage-app/src/components/ShiftChangeList/ShiftChangeList.tsx
@@ -10,34 +10,38 @@ import { Tooltip } from '@mui/material'
 
 interface PropsShiftList {
   shift_id: string
+  shiftChanges?: ShiftChange[] // Si le paso la propiedad shiftChanges al componente, no hace la solicitud al backend
 }
 
-function ShiftChangeList({ shift_id }: PropsShiftList) {
+function ShiftChangeList({ shift_id, shiftChanges: propShiftChanges }: PropsShiftList) {
   const { t } = useTranslation('ShiftList')
   const [sortColumn, setSortColumn] = useState<string | null>(null)
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('asc')
-  const [shiftChanges, setShiftChanges] = useState<ShiftChange[] | null>(null)
-  const [loading, setLoading] = useState<boolean>(true)
+  const [shiftChanges, setShiftChanges] = useState<ShiftChange[] | null>(propShiftChanges || null) // Modificar aquí
+  const [loading, setLoading] = useState<boolean>(!propShiftChanges)
   const [userNames, setUserNames] = useState<{ [key: string]: string }>({})
   const [patientNames, setPatientNames] = useState<{ [key: string]: string }>({})
   const token = Cookies.get('authToken')
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        if (token) {
-          const data = await getShiftChanges(null, null, shift_id)
-          console.log(data, 'data')
-          setShiftChanges(data)
+    if (!propShiftChanges) {
+      // Si al componente no se le pasa la propiedad shiftChanges hace la solicitud al backend
+      const fetchData = async () => {
+        try {
+          if (token) {
+            const data = await getShiftChanges(null, null, shift_id)
+            console.log(data, 'data')
+            setShiftChanges(data)
+          }
+        } catch (error) {
+          console.error((error as Error).message)
+        } finally {
+          setLoading(false)
         }
-      } catch (error) {
-        console.error((error as Error).message)
-      } finally {
-        setLoading(false)
       }
+      fetchData()
     }
-    fetchData()
-  }, [token, shift_id])
+  }, [token, shift_id, propShiftChanges])
 
   useEffect(() => {
     const fetchUserNames = async (userIds: string[]) => {
@@ -127,84 +131,99 @@ function ShiftChangeList({ shift_id }: PropsShiftList) {
       showOnLargeScreen: true,
       sortable: true
     },
-    { label: t('New Doctor ID'), field: 'new_doctor_id', showOnLargeScreen: true, sortable: true },
+    { label: t('New Doctor ID'), field: 'new_doctor_id', showOnSmallScreen: true, sortable: true },
     { label: t('Last Nurse ID'), field: 'last_nurse_id', showOnLargeScreen: true, sortable: true },
-    { label: t('New Nurse ID'), field: 'new_nurse_id', showOnLargeScreen: true, sortable: true },
-    { label: t('Patient ID'), field: 'patient_id', showOnLargeScreen: true, sortable: true },
+    { label: t('New Nurse ID'), field: 'new_nurse_id', showOnSmallScreen: true, sortable: true },
+    ...(!propShiftChanges
+      ? [
+          {
+            label: t('Patient ID'),
+            field: 'patient_id',
+            showOnLargeScreen: true,
+            sortable: true
+          }
+        ]
+      : []),
     {
       label: t('Patient Observations'),
       field: 'patient_observations',
-      showOnLargeScreen: true,
+      showOnSmallScreen: true,
       sortable: false
     },
     {
       label: t('Patient Records'),
       field: 'patient_records',
-      showOnLargeScreen: true,
+      showOnSmallScreen: true,
       sortable: false
     },
     {
       label: t('Patient Procedures'),
       field: 'patient_procedures',
-      showOnLargeScreen: true,
+      showOnSmallScreen: true,
       sortable: false
     }
   ]
 
   return (
-    <div className='mx-0 mt-4 lg:mx-8'>
-      <table className='w-full border border-gray-300'>
-        <thead>
-          <tr className='min-w-full bg-blue-800 text-white'>
-            {columns.map((column) => (
-              <th
-                key={column.field}
-                className={`border p-2 ${column.showOnLargeScreen ? '' : 'hidden lg:table-cell'}`}
-                onClick={() => (column.sortable ? handleSort(column.field) : null)}
-              >
-                {column.label}{' '}
-                {column.sortable && sortColumn === column.field && (
-                  <span>{sortDirection === 'asc' ? '▲' : '▼'}</span>
-                )}
-              </th>
-            ))}
-          </tr>
-        </thead>
-        <tbody className='text-center text-black'>
-          {sortedShiftChanges.map((shiftChange) => (
-            <tr key={shiftChange.id}>
+    <div className='mt-4 px-2 md:px-8'>
+      <div className='overflow-x-auto shadow-md rounded-lg'>
+        <table className='w-full text-sm md:text-base border border-gray-300'>
+          <thead>
+            <tr className='bg-blue-800 text-white'>
               {columns.map((column) => (
-                <td
+                <th
                   key={column.field}
-                  className={`border p-2 ${column.showOnLargeScreen ? '' : 'hidden lg:table-cell'}`}
+                  className={`p-2 text-left ${
+                    column.showOnSmallScreen ? '' : 'hidden lg:table-cell'
+                  } cursor-pointer text-center`}
+                  onClick={() => (column.sortable ? handleSort(column.field) : null)}
                 >
-                  {[
-                    'user_id',
-                    'last_doctor_id',
-                    'new_doctor_id',
-                    'last_nurse_id',
-                    'new_nurse_id'
-                  ].includes(column.field) ? (
-                    userNames[shiftChange[column.field as keyof ShiftChange]] || 'Loading...'
-                  ) : column.field === 'patient_id' ? (
-                    <Tooltip title={t('PatientInformation')}>
-                      <a
-                        href={`/patients/${shiftChange[column.field as keyof ShiftChange]}`}
-                        className='text-blue-500 underline'
-                      >
-                        {patientNames[shiftChange[column.field as keyof ShiftChange]] ||
-                          'Loading...'}
-                      </a>
-                    </Tooltip>
-                  ) : (
-                    shiftChange[column.field as keyof ShiftChange]
+                  {column.label}
+                  {column.sortable && sortColumn === column.field && (
+                    <span className='ml-1'>{sortDirection === 'asc' ? '▲' : '▼'}</span>
                   )}
-                </td>
+                </th>
               ))}
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {sortedShiftChanges.map((shiftChange) => (
+              <tr key={shiftChange.id}>
+                {columns.map((column) => (
+                  <td
+                    key={column.field}
+                    className={`border p-2 ${
+                      column.showOnSmallScreen ? '' : 'hidden lg:table-cell'
+                    } text-center`}
+                  >
+                    {[
+                      'user_id',
+                      'last_doctor_id',
+                      'new_doctor_id',
+                      'last_nurse_id',
+                      'new_nurse_id'
+                    ].includes(column.field) ? (
+                      userNames[shiftChange[column.field as keyof ShiftChange]] || 'Loading...'
+                    ) : column.field === 'patient_id' ? (
+                      <Tooltip title={t('PatientInformation')}>
+                        <a
+                          href={`/patients/${shiftChange[column.field as keyof ShiftChange]}`}
+                          className='text-blue-500 underline'
+                        >
+                          {patientNames[shiftChange[column.field as keyof ShiftChange]] ||
+                            'Loading...'}
+                        </a>
+                      </Tooltip>
+                    ) : (
+                      shiftChange[column.field as keyof ShiftChange]
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   )
 }
